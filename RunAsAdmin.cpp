14535a8069d18615c52dbf02ddd6959b2af014e3
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-10 13:54:38 -0400

    Message box will now popup after command execution

--- RunAsAdmin.cpp
+++ RunAsAdmin.cpp
@@ -1,4 +1,7 @@
 #include <format>
+#include <Windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 
 using namespace std;
 
@@ -7,6 +10,7 @@ int main(int argc, char* argv[])
     string command = format("cmd /min /C \"set __COMPAT_LAYER=RUNASINVOKER && start \"\" {0}\"", argv[1]);
 
     system(command.c_str());
+    MessageBox(0, _T("Executed program without admin rights!"), _T("Success!"), 0);
     
     return 0;
 }
