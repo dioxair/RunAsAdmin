617e381e94fa1f85f17911f570d9f942413d17db
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-10 13:59:27 -0400

    Added check to see if program has 2 args

--- RunAsAdmin.cpp
+++ RunAsAdmin.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    if (argv[1] == NULL)
+    {
+        MessageBox(0, _T("In order to use RunAsAdmin, you need to drag an exe on top of this exe!"), _T("Error!"), 0);
+        exit(1);
+    }
+
     string command = format("cmd /min /C \"set __COMPAT_LAYER=RUNASINVOKER && start \"\" {0}\"", argv[1]);
 
     system(command.c_str());
