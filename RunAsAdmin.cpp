06e28d1a615dd82394e7795110518ebada1deb7a
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-10 13:46:26 -0400

    Program will now execute command inside variable

--- RunAsAdmin.cpp
+++ RunAsAdmin.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     string command = format("cmd /min /C \"set __COMPAT_LAYER = RUNASINVOKER && start \"\" {0}\"", argv[1]);
+
+    system(command.c_str());
     
     return 0;
 }
